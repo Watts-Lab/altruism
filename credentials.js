548c17d0461a35e16b971b565aa9cd657356d8a7
Zhitong Chen <148817346+czt111@users.noreply.github.com>
2024-11-08 00:48:34 -0500

    Update credentials.js
    Signed-off-by: Zhitong Chen <148817346+czt111@users.noreply.github.com>

--- credentials.js
+++ credentials.js
@@ -1,5 +1,13 @@
-export const ACCESS_ID = "SOME_ACCESS_KEY"
-export const SECRET_KEY = "SOME_SECRET_KEY"
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const DDB_ACCESS_ID = "SOME_ACCESS_KEY"
-export const DDB_SECRET_KEY = "SOME_SECRET_KEY"
+// Retrieve credentials from environment variables
+const ACCESS_ID = process.env.ACCESS_ID;
+const SECRET_KEY = process.env.SECRET_KEY;
+const DDB_ACCESS_ID = process.env.DDB_ACCESS_ID;
+const DDB_SECRET_KEY = process.env.DDB_SECRET_KEY;
+
+// Export variables for use in other files with ES module syntax
+export const AWS_CONSOLE_URL = "https://aws-seas-wattslab-acct.signin.aws.amazon.com/console";
+export { ACCESS_ID, SECRET_KEY, DDB_ACCESS_ID, DDB_SECRET_KEY };
+
